095dff4a33e7b3adff40a52ffbac3238d06c95b3
Joe McLean <mclean.joe@gmail.com>
2013-07-11 11:29:22 -0400

    created basic models for resources and traits

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 IpiPlatform::Application.routes.draw do
+  resources :traits
+
+  resources :resources
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
