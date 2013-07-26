338c933dfcf7a5c9703ef27aa8214d4b4b1f3bf3
mikevarona <mikevarona@gmail.com>
2013-07-26 11:22:19 -0400

    capitalization of phases

--- app/controllers/phases_controller.rb
+++ app/controllers/phases_controller.rb
@@ -1,4 +1,4 @@
-class phasesController < ActionController::Base
+class PhasesController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
