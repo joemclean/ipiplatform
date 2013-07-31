68a9ad9c7cf8176dc0ea6005d6772b00d4b72914
Sung Yang <syang@thoughtworks.com>
2013-07-31 12:14:44 -0400

    Created full description field in resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130718141346) do
+ActiveRecord::Schema.define(version: 20130731160258) do
 
   create_table "resources", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130718141346) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_description"
   end
 
   create_table "suggestions", force: true do |t|
