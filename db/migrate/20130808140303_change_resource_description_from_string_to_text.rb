1b48b04e0350114ddfb548126b695df4e396cae0
Joe McLean <mclean.joe@gmail.com>
2013-08-08 10:10:16 -0400

    fixing bad migration

--- db/migrate/20130808140303_change_resource_description_from_string_to_text.rb
+++ db/migrate/20130808140303_change_resource_description_from_string_to_text.rb
@@ -1,4 +1,12 @@
 class ChangeResourceDescriptionFromStringToText < ActiveRecord::Migration
   def change
+    def up
+      change_column :resource, :full_description, :text
+    end
+    def down
+      # This might cause trouble if you have strings longer
+      # than 255 characters.
+      change_column :resource, :full_description, :string
+    end
   end
 end
