91246051e5fa989e5224bf6b1620b7cf2fefe323
Srikar K. Prasad <srikasip@gmail.com>
2013-08-08 14:26:48 -0400

    Show uploader name in tools, resources.show, and resources.index pages

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,4 +2,13 @@ class Resource < ActiveRecord::Base
   has_many :trait_associations
   has_many :traits, through: :trait_associations
   belongs_to :user
+
+  def author
+    if self.user_id
+      author = User.find(self.user_id)
+    else
+      author = User.new(name: 'IPI Team', email: 'ipi@sidekick.com')
+    end
+  end
+
 end
