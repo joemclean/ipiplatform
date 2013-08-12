c9600695c9be2fdc390089261578ecedb73c80ee
Joe McLean <mclean.joe@gmail.com>
2013-08-12 17:06:32 -0400

    Changing migrations to resolve Postgres type conversion issue

--- db/migrate/20130812144447_change_resource_industries_table_from_strings_to_integers.rb
+++ db/migrate/20130812144447_change_resource_industries_table_from_strings_to_integers.rb
@@ -1,10 +1,14 @@
 class ChangeResourceIndustriesTableFromStringsToIntegers < ActiveRecord::Migration
   def up
-    change_column :resource_industries, :resource_id, :integer
-    change_column :resource_industries, :industry_id, :integer
+    drop_column :resource_industries, :resource_id
+    create_column :resource_industries, :resource_id, :integer
+    drop_column :resource_industries, :industry_id
+    create_column :resource_industries, :industry_id, :integer
   end
   def down
-    change_column :resource_industries, :resource_id, :string
-    change_column :resource_industries, :industry_id, :string
+    drop_column :resource_industries, :resource_id
+    create_column :resource_industries, :resource_id, :string
+    drop_column :resource_industries, :resource_id
+    create_column :resource_industries, :resource_id, :string
   end
 end
