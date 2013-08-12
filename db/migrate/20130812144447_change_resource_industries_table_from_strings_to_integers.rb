da428f016b34ec81dcecb46af060167f56bfe1c2
Joe McLean <mclean.joe@gmail.com>
2013-08-12 17:14:14 -0400

    Resolving migration issues II

--- db/migrate/20130812144447_change_resource_industries_table_from_strings_to_integers.rb
+++ db/migrate/20130812144447_change_resource_industries_table_from_strings_to_integers.rb
@@ -1,14 +1,14 @@
 class ChangeResourceIndustriesTableFromStringsToIntegers < ActiveRecord::Migration
   def up
     remove_column :resource_industries, :resource_id
-    create_column :resource_industries, :resource_id, :integer
+    add_column :resource_industries, :resource_id, :integer
     remove_column :resource_industries, :industry_id
-    create_column :resource_industries, :industry_id, :integer
+    add_column :resource_industries, :industry_id, :integer
   end
   def down
     remove_column :resource_industries, :resource_id
-    create_column :resource_industries, :resource_id, :string
+    add_column :resource_industries, :resource_id, :string
     remove_column :resource_industries, :resource_id
-    create_column :resource_industries, :resource_id, :string
+    add_column :resource_industries, :resource_id, :string
   end
 end
